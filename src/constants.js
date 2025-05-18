93c8368cc5d195647a12d01e2210a847cb7e3e90
Abhishek <abhishekkanwasikannu15103@gmail.com>
2025-05-18 22:53:16 +0530

    yes

---
+++ src/constants.js
@@ -0,0 +1,246 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+
+import fusejackLogo from './assets/company_logo/fusejack_logo.png';
+
+
+// Education Section Logo's
+
+import ditLogo from './assets/education_logo/dit_logo.png';
+import cambrianHallLogo from './assets/education_logo/cambrianhall_logo.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+import smartvoyage from './assets/work_logo/smartvoyage.png';
+import himalayayatri from './assets/work_logo/himalayayatri.png';
+import lifelinkr from './assets/work_logo/lifelinkr.png';
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      // { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      // { name: 'Angular', logo: angularLogo },
+      // { name: 'Redux', logo: reduxLogo },
+      // { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      // { name: 'GSAP', logo: gsapLogo },
+      // { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      // { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      // { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      // { name: 'MongoDB', logo: mongodbLogo },
+      // { name: 'Firebase', logo: firebaseLogo },
+      // { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      // { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      // { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      // { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      // { name: 'Postman', logo: postmanLogo },
+      // { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      // { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: fusejackLogo,
+      role: "Web Developer",
+      company: "FuseJack",
+      date: "April 2024 - March 2025",
+      desc: "Developed dynamic and scalable web applications using the HTML, CSS, JS , handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "bootstrap",
+        "sql",
+        'php'
+      ],
+    },
+    // {
+    //   id: 1,
+    //   img: agcLogo,
+    //   role: "Fullstack Engineer",
+    //   company: "Agumentik Group of Companies",
+    //   date: "July 2023 - March 2024",
+    //   desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+    //   skills: [
+    //     "ReactJS",
+    //     "Redux",
+    //     "JavaScript",
+    //     "Tailwind CSS",
+    //     "HTML",
+    //     "CSS",
+    //     "SQL",
+    //   ],
+    // },
+    // {
+    //   id: 2,
+    //   img: newtonschoolLogo,
+    //   role: "Frontend Intern",
+    //   company: "Newton School",
+    //   date: "September 2021 - August 2022",
+    //   desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+    //   skills: [
+    //     "HTML",
+    //     "CSS",
+    //     "Javascript",
+    //     "Bootstrap",
+    //     "Figma",
+    //     "Material UI",
+    //   ],
+    // },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: ditLogo,
+      school: "DIT University, Dehradun",
+      date: "Sept 2021 - May 2025",
+      grade: "8.5 CGPA",
+      desc: "I completed my Bachelor's degree in Btech cse from DIT University, Dehradun. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+      degree: "Bachelor of Technology in Computer Science - B.Tech cse",
+    },
+    {
+      id: 1,
+      img: cambrianHallLogo,
+      school: "cambrian Hall School, Dehradun",
+      date: "Apr 2019 - March 2021",
+      grade: "90.5%",
+      desc: "I completed my class 12 education from Cambrian Hall School, Dehradun, under the ISC board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+      degree: "ISC(XII) - PCM with Computer Science",
+    },
+    {
+      id: 2,
+      img: cambrianHallLogo,
+      school: "Cambrian Hall School, Dehradun",
+      date: "Apr 2017 - March 2019",
+      grade: "89.4%",
+      desc: "I completed my class 10 education from Cambrian Hall School, Dehradun, under the ICSE board, where I studied Science with Computer.",
+      degree: "ICSE(X), Science with Computer Application",
+    },
+    // {
+    //   id: 3,
+      
+    // },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Smart Voyage- AI Travel Assistant",
+      description:
+        "Smart Voyage is an AI-powered travel assistant that helps users explore, understand, and plan trips with ease. It generates intelligent destination summaries, reviews, and travel itineraries using natural language processing and third-party data like Wikipedia and location services.",
+      image: smartvoyage,
+      tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+      github: "https://github.com/abhishekkanwasi/SMART_VOYAGE",
+      webapp: "https://smart-voyage.netlify.app/",
+    },
+    {
+      id: 1,
+      title: "Himalaya Yatri - Travel & Tour website",
+      description:
+        "Himalaya Yatri is a travel and tour website that offers a wide range of travel packages and services. It provides users with an easy-to-navigate interface to explore various destinations, book tours, and access travel-related information. The website is built using HTML, CSS, and JavaScript, PHP, and MySQL.",
+      image: himalayayatri,
+      tags: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"],
+      github: "https://himalayayatri.com/",
+      webapp: "https://himalayayatri.com/",
+    },
+    {
+      id: 2,
+      title: "LIFELINKR - a healthcare web app",
+      description:
+        "LIFELINKR is a healthcare web application that connects patients with doctors and healthcare professionals. It provides features like crm, why choose us, faq, contact us and more. The app is built using HTML, CSS, and JavaScript.",
+      image: lifelinkr,
+      tags: ["HTML", "CSS", "JavaScript"],
+      github: "https://github.com/abhishekkanwasi/lifelinkr",
+      webapp: "https://sunny-zuccutto-8d69ba.netlify.app/",
+    },
+    // {
+    //   id: 3,
+    //   title: "Email Validator NPM Package",
+    //   description:
+    //     "An efficient and customizable NPM package for validating email addresses. Built using React.js and Node.js, it provides robust validation features to help developers ensure that email inputs meet required formats and standards.",
+    //   image: npmLogo,
+    //   tags: ["React JS", "Node.js", "NPM", "Validation"],
+    //   github: "https://github.com/codingmastr/cmtk-email-validator",
+    //   webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+    // },
+    
+    
+  ];  
